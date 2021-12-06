9520ec9d8e168c8309d00462c719fd3690c892cb
Michael Fuchs <pubgrub@gmx.de>
2021-12-06 17:36:11 +0100

    Day 5 complete

--- day5.py
+++ day5.py
@@ -22,13 +22,19 @@ class Vent:
             return( [(x, self.y1) for x in range( min( self.x1, self.x2), max(self.x1, self.x2 ) +1)])
         if self.isVertical():
             return( [(self.x1, y) for y in range( min( self.y1, self.y2), max(self.y1, self.y2 ) +1)])
+        else:
+            xrange = range( self.x1, self.x2 +1, 1) if self.x2 > self.x1 else range( self.x1, self.x2 - 1, -1)
+            yrange = range( self.y1, self.y2 +1, 1) if self.y2 > self.y1 else range( self.y1, self.y2 - 1, -1)
+            l =  []
+            for i in range( 0, len(xrange)):
+                l.append( (xrange[i], yrange[i]))
+            return l
 
     def isHorizontal(self):
         return( self.y1 == self.y2)
 
     def isVertical(self):
         return( self.x1 == self.x2)
-
 
 #Task 1:
 
@@ -51,3 +57,25 @@ for key in grid:
         result += 1
 
 print( "Result Task 1: ", result)
+
+#Task 2:
+
+vents = []
+for l in lines:
+    vents.append( Vent(l))
+
+grid = {}
+for v in vents:
+    for p in v.getAllPoints():
+        try:
+            grid[ p] += 1
+        except KeyError:
+            grid[p] = 1
+
+result = 0
+for key in grid:
+    if grid[key] > 1:
+        result += 1
+
+print( "Result Task 2: ", result)
+
