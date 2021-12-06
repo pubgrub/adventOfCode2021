4a1c46025781d0b08da5392a66a3f861a9c1b3e8
pubgrub <pubgrub@gmx.de>
2021-12-06 13:14:21 +0000

    working on day 4

--- day04.py
+++ day04.py
@@ -2,11 +2,30 @@
 # Day 4
 
 #get input data
-sampleList = []
+from os import linesep
+
+
+lines = []
 with open( "04.data", "r") as file:
   for line in file:
-    sampleList.append( line.rstrip())
+    lines.append( line.rstrip())
 file.close()
+
+draws = lines[0].split( ",")
+del lines[0:2]
+print( lines[0])
+
+boards = []
+board = []
+for l in lines:
+   if len(l) != 0:
+     board.extend( int(i) for i in l.split())
+   else:
+     boards.append( board)
+     board = []
+if board != []:
+  boards.append( board)
+
 
 class BingoBoard:    
   
@@ -16,7 +35,35 @@ class BingoBoard:
       raise Exception( "Cannot create Board, wrong size. Expected: " + str(self.boardSize**2) + " got: " + str(len( ints)))
     self.numbers = ints[:]
     self.seen = [False] * len(self.numbers)
+    self.rows = []
+    for i in range(0,self.boardSize):
+      self.rows.append([] )
+      for j in range( 0, self.boardSize):
+        self.rows[-1].append( i*self.boardSize + j)
+    for i in range(0,self.boardSize):
+      self.rows.append([] )
+      for j in range( 0, self.boardSize):
+        self.rows[-1].append( i + j * self.boardSize)
 
+  def draw( self, num):
+    if num in self.numbers:
+      idx = self.numbers.index(num)
+      self.seen[idx] = True
+
+  def isSolved( self):
+    for r in self.rows:
+      try:
+         [self.seen[x] for x in r].index(False)
+      except:
+        return True
+    return False
+
+  def getValue( self):
+    idx = [i for i in range(0,len(self.numbers)) if self.seen[i] == False]
+    v = 0
+    for i in idx:
+      v += self.numbers[i]
+    return v
 
   def __str__(self):
     return( "Board: size -> " +  str(self.boardSize) +  
@@ -27,3 +74,12 @@ class BingoBoard:
 
 b = BingoBoard( 3, [17,58,52,49,72,33,55,73,27])
 print( b)
+b.draw( 51)
+b.draw( 52)
+print( b)
+print( b.isSolved())
+b.draw( 17)
+b.draw( 58)
+print( b.isSolved())
+print( b)
+print( b.getValue())
