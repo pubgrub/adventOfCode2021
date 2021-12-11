02d3d89997437491d397f87f9cf444340f88f5e0
Michael Fuchs <pubgrub@gmx.de>
2021-12-11 20:09:25 +0100

    Day 11 complete

---
+++ day11.py
@@ -0,0 +1,78 @@
+# AdventOfCode 2021
+# Day 11
+
+#get input data
+
+lines = []
+with open( "11.data", "r") as file:
+  for line in file:
+    lines.append( line.rstrip())
+file.close()
+
+bordererdGrid = [ -1] * 12
+for l in lines:
+  bordererdGrid.append( -1)
+  bordererdGrid = bordererdGrid + [int(x) for x in list(l)]
+  bordererdGrid.append( -1)
+bordererdGrid = bordererdGrid +  [ -1] * 12
+
+width = len(lines[0])
+neighors = [ -13, -12, -11, -1, 1, 11, 12, 13]
+steps = 100
+
+#print function for testing
+def printGrid( grid):
+  print( "\033[0m")
+  for i in range(1,11):
+    for j in range(1,11):
+      val = grid[ i * 12 + j]
+      valstr = "{0:3d}".format( val)
+      if  val == 0:
+        print( "\033[1;31m" + valstr + "\033[0m" , end = "")
+      else:
+        print( valstr, end = "")
+    print( "\033[0m")
+
+def raiseEnergy( oct, octs, has_flashed, flashCount):
+  octs[ oct] += 1
+  if octs[ oct] == 10:
+    has_flashed.add( oct)
+    octs[ oct] = 0
+    flashCount += 1
+    for n in neighors:
+      neighborPos = oct + n
+      if neighborPos < 0 or neighborPos >= len(octs) or octs[ neighborPos] == -1 or neighborPos in has_flashed:
+          continue
+      ( octs, has_flashed, flashCount) = raiseEnergy( neighborPos, octs, has_flashed, flashCount)
+  return ( octs, has_flashed, flashCount)   
+
+#Task 1
+
+octs = bordererdGrid[:]
+has_flashed = set()
+flashCount = 0
+
+for s in range( steps):
+  has_flashed.clear()
+  for o in range(len(octs)):
+    if octs[ o]  == -1 or o in has_flashed:
+      continue
+    ( octs, has_flashed, flashCount) = raiseEnergy( o, octs, has_flashed, flashCount)
+
+print( "Result Task 1: ", flashCount)
+
+#Task 2
+
+octs = bordererdGrid[:]
+has_flashed = set()
+
+step = 0
+while len(has_flashed) != width**2:
+  step += 1
+  has_flashed.clear()
+  for o in range(len(octs)):
+    if octs[ o]  == -1 or o in has_flashed:
+      continue
+    ( octs, has_flashed, flashCount) = raiseEnergy( o, octs, has_flashed, flashCount)
+
+print( "Result Task 2: ", step)
