1db9e20c57ebe4e95233a1fce674c10e86b5e248
pubgrub <pubgrub@gmx.de>
2021-12-16 14:42:31 +0000

    function solve()

--- day15.py
+++ day15.py
@@ -38,7 +38,8 @@ def walk( x, y, risk):
   if x == gridSizeX - 1 and y == gridSizeY - 1:
     if risk < minRisk:
       minRisk = risk
-      print( "new minimal risk found: ", minRisk)
+      #print( "new minimal risk found: ", minRisk)
+      print( ".", end='')
     return
   if (x + 1, y) not in path and x < gridSizeX - 1:
     walk( x + 1, y, risk)
@@ -53,21 +54,35 @@ def walk( x, y, risk):
     walk( x, y - 1, risk)
     path.popitem()
 
-#Task 1
+def solve( grid):
+  toCalc[ ( 1, 0)] = ''
+  toCalc[ ( 0, 1)] = ''
+  while len(toCalc) != 0:
+    ( x, y) =  next(iter(toCalc)) 
+    neighbors = []
+    if x > 0:
+      neighbors.append( (x-1, y))
+    if x < gridSizeX - 1:
+      neighbors.append( (x+1, y))
+    if y > 0:
+      neighbors.append( (x, y-1))
+    if y < gridSizeY - 1:
+      neighbors.append( (x, y+1))
+      
+    minRiskOfNeighbors = maxRisk
+    #print( x, y, neighbors, len( toCalc))
+    for (xn, yn) in neighbors:
 
-riskGrid = {} 
-path = {}
-minRisk = getInitialMinRisk( grid)
-import time
-start = time.time()
-walk( 0, 0, 0)
-end = time.time()
-
-print( "Result Task 1: ", minRisk - grid[0][0])
-print( end - start)
-exit()
-
-#Task 2
+      if ( xn, yn) in minPointRisks:
+        minRiskOfNeighbors = min( minRiskOfNeighbors, minPointRisks[ ( xn, yn)])
+    if ( x, y) not in minPointRisks or minRiskOfNeighbors < minPointRisks[ ( x, y)] - grid[y][x]:
+      #print( x, y)
+      minPointRisks[ ( x, y)] = minRiskOfNeighbors + grid[y][x]
+      for n in neighbors:
+        ( xn, yn) = n
+        if not n in minPointRisks or minPointRisks[ ( x, y)] + grid[ yn][ xn] < minPointRisks[ xn, yn]:
+          toCalc[ n] = ''
+    toCalc.pop( ( x, y))
 
 def buildLargeGrid( grid, mult):
   for i in range( 1, mult):
@@ -84,13 +99,47 @@ def buildLargeGrid( grid, mult):
         grid[-1].append( v - 9 if v > 9 else v)
   return grid
 
+
+import time
+#Task 1
+
+minPointRisks = { ( 0,0): 0}
+toCalc = {}
+maxRisk = getInitialMinRisk(grid)
+minRisk = maxRisk
+
+riskGrid = {} 
+path = {}
+
+# slowVersion
+#walk( 0, 0, 0)
+#print( "Result Task 1: ", minRisk - grid[0][0])
+
+# fastVersion
+
+start1 = time.time()
+solve( grid)
+end1 = time.time()
+print( minPointRisks[ ( gridSizeX - 1, gridSizeY - 1)])
+print( end1 -start1)
+#Task 2
+
 grid = buildLargeGrid( grid, 5)
 gridSizeY = len( grid)
 gridSizeX = len( grid[0])
 
+minPointRisks = { ( 0,0): 0}
+toCalc = {}
+maxRisk = getInitialMinRisk(grid)
+minRisk = maxRisk
+
 riskGrid = {} 
 path = {}
-minRisk = getInitialMinRisk( grid)
-walk( 0, 0, 0)
 
-print( "Result Task 2: ", minRisk - grid[0][0])
+start2 = time.time()
+solve( grid)
+end2 = time.time()
+
+print( minPointRisks[ ( gridSizeX - 1, gridSizeY - 1)])
+print( end2 - start2)
+
