9ee4132411bffd43ba5a755a053108c84d94bb5f
Michael Fuchs <pubgrub@gmx.de>
2021-12-19 19:32:40 +0100

    cleanup

--- day18.py
+++ day18.py
@@ -9,10 +9,10 @@ with open( "18.data", "r") as file:
     lines.append( line.rstrip())
 file.close()
 
-class Number:
+class Pair:
   
   @staticmethod
-  def buildTree( str, num):
+  def buildNumber( str, num):
     while str:
       if str[:1].isdigit(): # handle numbers > 9
         i = 1
@@ -22,7 +22,7 @@ class Number:
       else:
         s, str = str[ :1], str[ 1:]
       if s == '[':
-        num = Number( num)
+        num = Pair( num)
       elif s == ',' or s == ' ':
         continue
       elif s == ']':
@@ -35,8 +35,7 @@ class Number:
   def __init__( self, parent):
     self.children = []
     self.parent = parent
-    self.magnitude = 0
-    if parent.__class__ == Number:
+    if parent.__class__ == Pair:
       parent.addChild( self)
 
   def getMagnitude(self):
@@ -47,18 +46,6 @@ class Number:
       mag += mult[ i] * (c if c.__class__ == int else c.getMagnitude())
     return mag
 
-  def getLeftChild( self):
-    return self.children[0]
-
-  def setLeftChild( self, child):
-    self.children[0] = child
-
-  def getRightChild( self):
-    return self.children[1]
-
-  def setRightChild( self, child):
-    self.children[1] = child
-
   def addChild( self, child):
     self.children.append( child)
 
@@ -68,34 +55,27 @@ class Number:
       changed = False
       toExplode = self.searchForExplodingNumber()
       if toExplode:
-        #print( "before ex: ", self.oneLineString(True), " -> ", toExplode.oneLineString())
         toExplode.explodeNumber()
-        #print( "after  ex: ", self.oneLineString())
-        #print( "after  ex: ", self.oneLineString(True))
-
         changed = True
         continue
       toSplit = self.searchForSplittingNumber()
       if toSplit:
-        #print( "before sp: ", self.oneLineString(), " -> ", toSplit.oneLineString())
         toSplit.splitNumber()
-        #print( "after  sp: ", self.oneLineString())
         changed = True
-
+    return self
 
   def searchForExplodingNumber( self, lvl = 0):
     if lvl == 4:
       return self
     else:
       for c in self.children:
-        if c.__class__ == Number:
+        if c.__class__ == Pair:
           found = c.searchForExplodingNumber( lvl + 1)
-          if found.__class__ == Number:
+          if found.__class__ == Pair:
             return found
     return False  
 
   def explodeNumber( self):
-    
     for i in range( len( self.children)):
       self.parent.addExplodeValue( i, 1, self.children[i], self)
     for i in range( len(self.parent.children)):
@@ -104,12 +84,11 @@ class Number:
 
   def addExplodeValue(  self, lr, upDown, value, sender):
       if upDown: #going up
-        #print( "***", lr, upDown, value, self.children)
-        if self.children[ lr].__class__  != Number:
+        if self.children[ lr].__class__  != Pair:
           self.children[ lr] += value
           return
         elif self.children[ lr] == sender:  
-          # coming up from the left
+          # coming up from that side
           if self.parent != None:
             self.parent.addExplodeValue( lr, upDown, value, self)
           else:
@@ -121,7 +100,7 @@ class Number:
           upDown = 0
           self.children[ dir].addExplodeValue( lr, upDown, value, self)
       else:
-        if self.children[ lr].__class__ != Number:
+        if self.children[ lr].__class__ != Pair:
           self.children[ lr] += value
           return
         else:
@@ -132,9 +111,7 @@ class Number:
     found = False
     for i in self.children:
       if i.__class__ == int:
-        #print( "found int")
         if i > 9:
-          #print( "found >9")
           return self
       else:
         found = i.searchForSplittingNumber()
@@ -147,7 +124,7 @@ class Number:
     for i in range( len(self.children)):
       child = self.children[ i]
       if child.__class__ == int and child > 9:
-        newNumber = Number( None)
+        newNumber = Pair( None)
         self.children[i] = newNumber
         newNumber.parent = self
         child0 = int( child // 2)
@@ -157,7 +134,7 @@ class Number:
         return
 
   def addNumber( self, num):
-    newNumber = Number( None)
+    newNumber = Pair( None)
     newNumber.addChild( self)
     newNumber.addChild( num)
     self.parent = newNumber
@@ -167,18 +144,16 @@ class Number:
   def __str__( self, level = 0):
     ret = "\t" * level + "Number:\n"
     for c in self.children:
-      if c.__class__ == Number:
+      if c.__class__ == Pair:
         ret += c.__str__( level + 1)
       else:
         ret += "\t" * level + c.__str__() + '\n'
-    if level == 0:
-      ret += self.oneLineString()
     return ret
 
   def oneLineString(self, colored = False):
     ret = '['
     for c in range( len(self.children)):
-      ret += self.children[ c].oneLineString() if self.children[ c].__class__ == Number else str( self.children[ c])
+      ret += self.children[ c].oneLineString() if self.children[ c].__class__ == Pair else str( self.children[ c])
       if c == 0:
         ret += ','
     ret += ']'
@@ -197,31 +172,18 @@ class Number:
           if lvl >= 5:
             color[ c] = 0
         if ret[c].isdigit() and ret[c+1].isdigit():
-          color[ c] = 1
-          color[ c + 1] = 1
-
+          color[ c] = color[ c + 1] = 1
       for r in reversed(sorted( color)):
         ret = ret[:r] + colorString[ color[ r]] + ret[r] + colorOffString + ret[r+1:]
-        #if color:
-          #print( ret)
-
     return ret
 
 
 #Task 1
 
-l = lines[0]
-tree = Number.buildTree( l, None)
-
+num = Pair.buildNumber( lines[0], None)
 for l in lines[ 1:]:
-  addTree = Number.buildTree( l, None)
-  #print( "both:  ", tree.oneLineString(), addTree.oneLineString())
-  tree = tree.addNumber( addTree)
-  #print( "added: ", tree.oneLineString())
-  tree.reduce()
-  #print( "redcd: ", tree.oneLineString())
-#print( tree)
-print( "Result Task 1: ", tree.getMagnitude())
+  num = num.addNumber( Pair.buildNumber( l, None)).reduce()
+print( "Result Task 1: ", num.getMagnitude())
 
 #Task 2
 
@@ -230,10 +192,7 @@ for i in lines:
   for j in lines:
     if j == i:
       continue
-    num1 = Number.buildTree( i, None)
-    num2 = Number.buildTree( j, None)
-    num = num1.addNumber( num2)
-    num.reduce()
+    num = Pair.buildNumber( i, None).addNumber( Pair.buildNumber( j, None)).reduce()
     mag = num.getMagnitude()
     maxMagnitude = max( maxMagnitude, mag)
 print( "Result Task 2: ", maxMagnitude)
